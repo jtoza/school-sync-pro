7a969f34ecf045557d4c065bc21c1ecccf4771ec
jtoza <jtozaoza@gmail.com>
2025-11-16 22:15:50 -0800

    Fixed modals blinking issue and updated finance URLs

--- apps/finance/urls.py
+++ apps/finance/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path("<int:pk>/detail/", InvoiceDetailView.as_view(), name="invoice-detail"),
     path("<int:pk>/update/", InvoiceUpdateView.as_view(), name="invoice-update"),
     path("<int:pk>/delete/", InvoiceDeleteView.as_view(), name="invoice-delete"),
-    path("receipt/create", ReceiptCreateView.as_view(), name="receipt-create"),
+    path("receipt/create/", ReceiptCreateView.as_view(), name="receipt-create"),
     path(
         "receipt/<int:pk>/update/", ReceiptUpdateView.as_view(), name="receipt-update"
     ),
